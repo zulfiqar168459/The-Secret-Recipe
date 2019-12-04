de919a8fceebef5a37dd0da28f17f8c468ba0fbf
zulfiqar168459 <38880762+zulfiqar168459@users.noreply.github.com>
2019-12-04 18:38:06 +0200

    zulfiqar

--- The secret recipe 4/Assets/Script/gravityswitch.cs
+++ The secret recipe 4/Assets/Script/gravityswitch.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class gravityswitch : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public bool Top;
+
     void Start()
     {
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.L)) { rb.gravityScale *= -1; }
+        Rotation();
+    }
+
+    void Rotation()
+    {
+        if (!Top)
+        {
+            transform.eulerAngles = new Vector3(0,0,180f);
+        }
+
+        else
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+
+        Top = !Top;
+
     }
 }
